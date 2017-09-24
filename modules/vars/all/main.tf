94133c0e03d793e3775ae71b23d09962f47d36c9
Josiah Niedrauer <jniedrauer@granburyrs.com>
2017-09-23 21:12:01 -0500

    Add vars module

---
+++ modules/vars/all/main.tf
@@ -0,0 +1,30 @@
+variable "azs" {
+    default = {
+        us-east-1 = "us-east-1a,us-east-1b,us-east-1c,us-east-1d,us-east-1e,us-east-1f"
+        us-west-2 = "us-west-2a,us-west-2b,us-west-2c"
+    }
+}
+output "azs" { value = "${var.azs}" }
+
+variable "amzn_amis" {
+    default = {
+        us-east-1 = "ami-a4c7edb2"
+    }
+}
+output "amzn_amis" { value = "${var.amzn_amis}" }
+
+variable "ssh_cidrs" {
+    default = [
+        "76.183.216.168/32"
+    ]
+}
+output "ssh_cidrs" { value = "${var.ssh_cidrs}" }
+
+variable "protocol_ports" {
+    default = {
+        ssh = "22"
+        http = "80"
+        https = "443"
+    }
+}
+output "protocol_ports" { value = "${var.protocol_ports}" }
