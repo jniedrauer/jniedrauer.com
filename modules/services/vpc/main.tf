931c66a7617a8475ccb554531f2ccf4af2de33de
Josiah Niedrauer <jniedrauer@granburyrs.com>
2017-09-23 21:23:51 -0500

    Stop using default ACL

--- modules/services/vpc/main.tf
+++ modules/services/vpc/main.tf
@@ -10,8 +10,40 @@ resource "aws_internet_gateway" "main" {
 	vpc_id = "${aws_vpc.main.id}"
 }
 
-resource "aws_default_network_acl" "default" {
-    default_network_acl_id = "${aws_vpc.main.default_network_acl_id}"
+resource "aws_subnet" "public" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "${var.public_subnets[count.index]}"
+    availability_zone = "${element(split(",", lookup(module.static.azs, var.aws_config["region"])), count.index)}"
+    map_public_ip_on_launch = "true"
+    tags {
+        Name = "public${count.index}"
+        Type = "public"
+    }
+    count = "${length(var.public_subnets)}"
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = "${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.main.id}"
+    }
+    route {
+        ipv6_cidr_block = "::/0"
+        gateway_id = "${aws_internet_gateway.main.id}"
+    }
+    count = "${length(var.public_subnets)}"
+}
+
+resource "aws_route_table_association" "public" {
+    subnet_id = "${aws_subnet.public.*.id[count.index]}"
+    route_table_id = "${aws_route_table.public.*.id[count.index]}"
+    count = "${length(var.public_subnets)}"
+}
+
+resource "aws_network_acl" "default" {
+    vpc_id = "${aws_vpc.main.id}"
+    subnet_ids = ["${aws_subnet.public.*.id}"]
     egress {
         protocol = "-1"
         rule_no = 200
@@ -44,37 +76,6 @@ resource "aws_default_network_acl" "default" {
         from_port  = 0
         to_port    = 0
     }
-}
-
-resource "aws_subnet" "public" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "${var.public_subnets[count.index]}"
-    availability_zone = "${element(split(",", lookup(module.static.azs, var.aws_config["region"])), count.index)}"
-    map_public_ip_on_launch = "true"
-    tags {
-        Name = "public${count.index}"
-        Type = "public"
-    }
-    count = "${length(var.public_subnets)}"
-}
-
-resource "aws_route_table" "public" {
-    vpc_id = "${aws_vpc.main.id}"
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.main.id}"
-    }
-    route {
-        ipv6_cidr_block = "::/0"
-        gateway_id = "${aws_internet_gateway.main.id}"
-    }
-    count = "${length(var.public_subnets)}"
-}
-
-resource "aws_route_table_association" "public" {
-    subnet_id = "${aws_subnet.public.*.id[count.index]}"
-    route_table_id = "${aws_route_table.public.*.id[count.index]}"
-    count = "${length(var.public_subnets)}"
 }
 
 output "id" {
