d1324af3f24c93fdfa031eab92c3a6b12259760e
Josiah Niedrauer <jniedrauer@granburyrs.com>
2017-10-01 08:04:15 -0500

    Add guestbook to index

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,11 @@
 from flask import jsonify, render_template, request
-from ..main import app
+from ..main import app, db
+from dictalchemy import make_class_dictable
+make_class_dictable(db.Model)
+
 from .api import get_client_ip
+
+from .models import Guest
 
 
 @app.errorhandler(404)
@@ -10,7 +15,8 @@ def not_found(error):
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Hello World!')
+    guests = [r.asdict() for r in Guest.query.all()]
+    return render_template('index.html', guests=guests)
 
 
 @app.route('/ip')
