7913b801908084866c5018a0f47300cd9df95503
Josiah Niedrauer <jniedrauer@granburyrs.com>
2017-10-01 01:44:08 -0500

    Add www aliases

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -15,7 +15,7 @@ resource "aws_ecr_repository" "ecr" {
 data "template_file" "container-definition" {
     template = "${file("${path.module}/task-definitions/service.json")}"
     vars {
-        image = "${aws_ecr_repository.ecr.repository_url}:latest"
+        image = "${aws_ecr_repository.ecr.repository_url}"
     }
 }
 
@@ -183,6 +183,18 @@ resource "aws_route53_record" "jniedrauer" {
     }
 }
 
+resource "aws_route53_record" "www-jniedrauer" {
+    zone_id = "${module.static.jniedrauer_zone_id}"
+    name = "www.jniedrauer.com"
+    type = "A"
+
+    alias {
+        name = "jniedrauer.com"
+        zone_id = "${module.static.jniedrauer_zone_id}"
+        evaluate_target_health = false
+    }
+}
+
 resource "aws_route53_record" "josiahniedrauer" {
     zone_id = "${module.static.josiahniedrauer_zone_id}"
     name = "josiahniedrauer.com"
@@ -191,6 +203,18 @@ resource "aws_route53_record" "josiahniedrauer" {
     alias {
         name = "${aws_elb.ecs.dns_name}"
         zone_id = "${aws_elb.ecs.zone_id}"
+        evaluate_target_health = false
+    }
+}
+
+resource "aws_route53_record" "www-josiahniedrauer" {
+    zone_id = "${module.static.josiahniedrauer_zone_id}"
+    name = "www.josiahniedrauer.com"
+    type = "A"
+
+    alias {
+        name = "josiahniedrauer.com"
+        zone_id = "${module.static.josiahniedrauer_zone_id}"
         evaluate_target_health = false
     }
 }
