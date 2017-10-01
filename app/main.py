a35a98985ace8a2dc8c4a83ae30c62cc493c06bf
Josiah Niedrauer <jniedrauer@granburyrs.com>
2017-09-30 23:11:09 -0500

    Add robots.txt and serve static content from root

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 if os.environ.get('FLASK_CONFIG'):
     app.config.from_envvar('FLASK_CONFIG')
