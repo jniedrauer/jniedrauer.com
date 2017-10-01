1d7f9f5397e40a8cfd0c7cdd84a63621d8f63304
Josiah Niedrauer <jniedrauer@granburyrs.com>
2017-10-01 07:09:36 -0500

    Implement status codes into API

--- app/core/api.py
+++ app/core/api.py
@@ -19,17 +19,19 @@ def guestbook():
     """Allow guests to view or post to the guestbook"""
     sucess = False
     reason = "You shouldn't ever see this"
+    code = 200
 
     if request.method == 'POST':
         data = request.get_json(force=True)
         app.logger.debug('/api/guestbook recieved POST: %s', data)
         if data.get('name'):
-            success, reason = add_guest(data['name'])
+            success, reason, code = add_guest(data['name'])
         else:
             success = False
             reason = 'RTFM'
+            code = 400
 
-        return api_reply({'acknowledged': True}, success=success, reason=reason)
+        return api_reply({'acknowledged': True}, success=success, reason=reason), code
     else:
         guests = None
         try:
@@ -39,8 +41,9 @@ def guestbook():
         except SQLAlchemyError:
             success = False
             reason = 'Storage read error'
+            code = 500
 
-        return api_reply({'guests': guests}, success=success, reason=reason)
+        return api_reply({'guests': guests}, success=success, reason=reason), code
 
 
 def get_client_ip():
@@ -54,11 +57,11 @@ def add_guest(name):
     db.session.add(guest)
     try:
         db.session.commit()
-        return True, None
+        return True, None, 200
     except IntegrityError:
-        return False, 'Doh! You already signed'
+        return False, 'Doh! You already signed', 400
     except SQLAlchemyError:
-        return False, 'Storage engine error'
+        return False, 'Storage engine error', 500
 
 
 def api_reply(body={}, success=True, reason=None):
