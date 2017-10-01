39fbce2a0e225591423f9b2d1edd111822063466
Josiah Niedrauer <jniedrauer@granburyrs.com>
2017-10-01 04:54:01 -0500

    Add a standard API reply interface

--- app/core/api.py
+++ app/core/api.py
@@ -4,8 +4,15 @@ from ..main import app
 
 @app.route('/api/ip')
 def api_ip():
-    return jsonify({'Success': True, 'ipAddress': get_client_ip()})
+    """Return client IP"""
+    return api_reply({'ipAddress': get_client_ip()})
 
 
 def get_client_ip():
+    """Return the client x-forwarded-for header or IP address"""
     return request.headers.get('X-Forwarded-For') or request.remote_addr
+
+
+def api_reply(body={}, success=True):
+    """Create a standard API reply interface"""
+    return jsonify({**body, 'success': success})
